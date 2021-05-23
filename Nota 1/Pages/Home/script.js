b54e02b148545de5028ed4432b318abf0a5406f7
Danilo Silva de Oliveira <danilo.silva.oliveira0021@gmail.com>
2021-05-23 19:12:23 -0300

    Finalizando a pagina home

--- Nota 1/Pages/Home/script.js
+++ Nota 1/Pages/Home/script.js
@@ -72,3 +72,29 @@ function toggleMenu() {
 
     document.getElementById("procura-mobile").innerHTML = procurar
 }
+
+function toggleAbout() {
+    const about = document.getElementById("about-mobile");
+    var info = 
+    `
+    <div class="modal" tabindex="-1">
+    <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+    `
+
+    about.innerHTML = info
+}
